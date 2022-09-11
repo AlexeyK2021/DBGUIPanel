11eaec57d4a9c6bd1b7295f3a09cc757a195b57d
AlexeyK2021 <kalashnickov.alexey2015@yandex.ru>
2022-09-11 14:13:58 +0300

    Second Init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.dbguipanel {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ru.alexeyk2021.dbguipanel to javafx.fxml;
+    exports ru.alexeyk2021.dbguipanel;
+}
---
+++ src/main/java/ru/alexeyk2021/dbguipanel/HelloController.java
@@ -0,0 +1,14 @@
+package ru.alexeyk2021.dbguipanel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
