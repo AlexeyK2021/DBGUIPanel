80b105226a028652873c005bd6f7bfd7af4daec6
AlexeyK2021 <kalashnickov.alexey2015@yandex.ru>
2022-10-14 14:52:48 +0300

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/ru/alexeyk2021/dbguipanel/models/Client.java
+++ src/main/java/ru/alexeyk2021/dbguipanel/models/Client.java
@@ -2,8 +2,8 @@ package ru.alexeyk2021.dbguipanel.models;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
 
+@Entity
 public class Client {
     private int clientId;
     private double balance;
